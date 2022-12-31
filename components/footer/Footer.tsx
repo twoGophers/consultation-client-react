c5cc69ab70a3a061c8c15de30e14915d70eececa
Vladimir <falinvlad93@rambler.ru>
2022-12-31 13:55:27 +0300

    fix build

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -10,13 +10,13 @@ export default function Footer() {
        <div className="footer container">
             <p>Наши контакты</p>
             <div className="footer__link">
-                <a href='tel: 89776115779' target='_blank'>
+                <a href='tel: 89776115779' target='_blank' rel="noopener noreferrer" >
                     <MailOutlineIcon sx={{ fontSize: 32 }} />
                 </a>
                 <a href="mailto:koptelovavictory@yandex.ru">
                     <PhoneIphoneIcon sx={{ fontSize: 32 }} />
                 </a>
-                <a href='tel: 89776115779' target='_blank'>
+                <a href='tel: 89776115779' target='_blank' rel="noopener noreferrer" >
                     <InstagramIcon sx={{ fontSize: 32 }} />
                 </a>
             </div>
