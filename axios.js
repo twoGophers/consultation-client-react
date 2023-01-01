ef75a63005844aab5052532aed6128f5aab0f052
Vladimir <falinvlad93@rambler.ru>
2023-01-01 14:15:48 +0300

    fix

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://consultation-server-react.vercel.app',
+  baseURL: 'https://consultation-server-react.vercel.app/',
 });
 
 export default instance ;
