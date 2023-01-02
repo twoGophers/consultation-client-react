ca0e18a058a32fda4b09cdc4e2f9cfa46438674b
Vladimir <falinvlad93@rambler.ru>
2023-01-02 12:02:50 +0300

    fix url telegram

--- store/slice/TelegramSlice.jsx
+++ store/slice/TelegramSlice.jsx
@@ -7,7 +7,7 @@ const bot_id = process.env.NEXT_PUBLIC_TELEGRAM_BOT_ID;
 export const fetchChatBot = createAsyncThunk('auth/fetchChatBot', async (params) => {
   console.log(token);
   console.log(bot_id);
-  console.log(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${bot_id}&parse_mode=html&text=${params}`);
+  console.log(`https://api.telegram.org/bot5912961863:AAG-LXjlL5ldZqjFWzghgpRjJ9uUoVU00cI/sendMessage?chat_id=998384982&parse_mode=html&text=${params}`);
   const { bot } = await axios.post(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${bot_id}&parse_mode=html&text=${params}`,"r");
   return bot;
 });
