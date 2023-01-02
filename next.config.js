39a596e9ad1b5e296c23769b7b02064b9fb42cbb
Vladimir <falinvlad93@rambler.ru>
2023-01-02 13:03:15 +0300

    change url axios

--- next.config.js
+++ next.config.js
@@ -5,3 +5,12 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    TOKEN_BOT_API: process.env.TOKEN_BOT_API,
+    TELEGRAM_BOT_ID: process.env.TELEGRAM_BOT_ID,
+  }
+}
